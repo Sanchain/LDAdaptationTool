be122c60f8fc9e06db20706703ba7956517d59cc
Sanchain <954598055@qq.com>
2021-02-20 10:32:00 +0800

    新增了SE ,IPHONE 12的机型

--- LDAdaptationToolDemo/LDAdaptationTool/LDAdaptationTool.m
+++ LDAdaptationToolDemo/LDAdaptationTool/LDAdaptationTool.m
@@ -127,7 +127,7 @@
     struct utsname systemInfo;
     uname(&systemInfo);
     NSString * deviceString = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
-//    NSLog(@"DeviceString : %@", deviceString);
+    NSLog(@"Device Model : %@", deviceString);
     //iPhone
     if ([deviceString isEqualToString:@"iPhone1,1"])    return @"iPhone 1G";
     if ([deviceString isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
@@ -136,29 +136,43 @@
     if ([deviceString isEqualToString:@"iPhone3,2"])    return @"iPhone 4";
     if ([deviceString isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
     if ([deviceString isEqualToString:@"iPhone5,1"])    return @"iPhone 5";
-    if ([deviceString isEqualToString:@"iPhone5,2"])    return @"iPhone 5";
-    if ([deviceString isEqualToString:@"iPhone5,3"])    return @"iPhone 5C";
-    if ([deviceString isEqualToString:@"iPhone5,4"])    return @"iPhone 5C";
-    if ([deviceString isEqualToString:@"iPhone6,1"])    return @"iPhone 5S";
-    if ([deviceString isEqualToString:@"iPhone6,2"])    return @"iPhone 5S";
+    if ([deviceString isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (GSM+CDMA)";
+    if ([deviceString isEqualToString:@"iPhone5,3"])    return @"iPhone 5c (GSM)";
+    if ([deviceString isEqualToString:@"iPhone5,4"])    return @"iPhone 5c (GSM+CDMA)";
+    if ([deviceString isEqualToString:@"iPhone6,1"])    return @"iPhone 5s (GSM)";
+    if ([deviceString isEqualToString:@"iPhone6,2"])    return @"iPhone 5s (GSM+CDMA)";
     if ([deviceString isEqualToString:@"iPhone7,1"])    return @"iPhone 6 Plus";
     if ([deviceString isEqualToString:@"iPhone7,2"])    return @"iPhone 6";
     if ([deviceString isEqualToString:@"iPhone8,1"])    return @"iPhone 6s";
     if ([deviceString isEqualToString:@"iPhone8,2"])    return @"iPhone 6s Plus";
-    if ([deviceString isEqualToString:@"iPhone9,1"])    return @"iPhone 7";
-    if ([deviceString isEqualToString:@"iPhone9,2"])    return @"iPhone 7 Plus";
-    if ([deviceString  isEqualToString:@"iPhone10,1"])   return @"iPhone 8";
-    if ([deviceString  isEqualToString:@"iPhone10,4"])   return @"iPhone 8";
-    if ([deviceString  isEqualToString:@"iPhone10,2"])   return @"iPhone 8 Plus";
-    if ([deviceString  isEqualToString:@"iPhone10,5"])   return @"iPhone 8 Plus";
-    if ([deviceString  isEqualToString:@"iPhone10,3"])   return @"iPhone X";
-    if ([deviceString  isEqualToString:@"iPhone11,2"])   return @"iPhone XS";
-    if ([deviceString  isEqualToString:@"iPhone11,4"])   return @"iPhone XS Max";
-    if ([deviceString  isEqualToString:@"iPhone11,6"])   return @"iPhone XS Max";
-    if ([deviceString  isEqualToString:@"iPhone11,8"])   return @"iPhone XR";
-    if ([deviceString  isEqualToString:@"iPhone12,1"])   return @"iPhone 11";
-    if ([deviceString  isEqualToString:@"iPhone12,3"])   return @"iPhone 11 Pro";
-    if ([deviceString  isEqualToString:@"iPhone12,5"])   return @"iPhone 11 Pro Max";
+    if ([deviceString isEqualToString:@"iPhone8,4"])    return @"iPhone SE";
+    if ([deviceString isEqualToString:@"iPhone9,1"])    return @"国行、日版、港行iPhone 7";
+    if ([deviceString isEqualToString:@"iPhone9,2"])    return @"港行、国行iPhone 7 Plus";
+    if ([deviceString isEqualToString:@"iPhone9,3"])    return @"美版、台版iPhone 7";
+    if ([deviceString isEqualToString:@"iPhone9,4"])    return @"美版、台版iPhone 7 Plus";
+    if ([deviceString isEqualToString:@"iPhone10,1"])   return @"国行(A1863)、日行(A1906)iPhone 8";
+    if ([deviceString isEqualToString:@"iPhone10,3"])   return @"iPhone X";
+    if ([deviceString isEqualToString:@"iPhone10,4"])   return @"美版(Global/A1905)iPhone 8";
+    if ([deviceString isEqualToString:@"iPhone10,2"])   return @"国行(A1864)、日行(A1898)iPhone 8 Plus";
+    if ([deviceString isEqualToString:@"iPhone10,5"])   return @"美版(Global/A1897)iPhone 8 Plus";
+    if ([deviceString isEqualToString:@"iPhone10,3"])   return @"国行(A1865)、日行(A1902)iPhone X";
+    if ([deviceString isEqualToString:@"iPhone10,6"])   return @"美版(Global/A1901)iPhone X";
+    
+    if ([deviceString isEqualToString:@"iPhone11,2"])   return @"iPhone XS";
+    if ([deviceString isEqualToString:@"iPhone11,4"])   return @"iPhone XS Max";
+    if ([deviceString isEqualToString:@"iPhone11,6"])   return @"iPhone XS Max";
+    if ([deviceString isEqualToString:@"iPhone11,8"])   return @"iPhone XR";
+    if ([deviceString isEqualToString:@"iPhone12,1"])   return @"iPhone 11";
+    if ([deviceString isEqualToString:@"iPhone12,3"])   return @"iPhone 11 Pro";
+    if ([deviceString isEqualToString:@"iPhone12,5"])   return @"iPhone 11 Pro Max";
+    if ([deviceString isEqualToString:@"iPhone12,8"])   return @"iPhone SE 2";
+    if ([deviceString isEqualToString:@"iPhone13,1"])   return @"iPhone 12 mini";
+    if ([deviceString isEqualToString:@"iPhone13,2"])   return @"iPhone 12";
+    if ([deviceString isEqualToString:@"iPhone13,3"])   return @"iPhone 12 Pro";
+    if ([deviceString isEqualToString:@"iPhone13,4"])   return @"iPhone 12 Pro Max";
+
+    if ([deviceString isEqualToString:@"i386"])         return @"Simulator";
+    if ([deviceString isEqualToString:@"x86_64"])       return @"Simulator";
 
     return deviceString;
 }
